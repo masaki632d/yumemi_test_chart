d80467356c2eaa926e29d61557837db2bdcaaf3b
koumura <masaki632d@gmail.com>
2023-01-21 13:13:12 +0900

    add: first settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import '@src/styles/globals.css'
+import 'ress'
+import '@styles/globals.scss'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
